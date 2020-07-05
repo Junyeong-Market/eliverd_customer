010d4e9d681a1723037979c60489484e7d068bcd
unperknown <unperknown@icloud.com>
2020-07-05 17:43:14 +0900

    feat: completed google maps & geolocation

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,14 +11,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Home',
-        ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        brightness: Brightness.light,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
       ),
+      body: FutureBuilder<CameraPosition>(
+        future: _getCurrentLocation(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: snapshot.data,
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  snapshot.error,
+                ),
+              );
+            }
+
+            return Center(
+              child: CupertinoActivityIndicator(),
+            );
+          },
+      ),
+    );
+  }
+
+  Future<CameraPosition> _getCurrentLocation() async {
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
+
+    return CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: 20.0,
     );
   }
 }
