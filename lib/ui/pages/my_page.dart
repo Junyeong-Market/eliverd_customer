3eb1ce2cc4efaa7099fa257ed2dfdce4e6487bbd
unperknown <unperknown@icloud.com>
2020-07-11 23:24:47 +0900

    feat: my page

---
+++ lib/ui/pages/my_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:Eliverd/ui/widgets/header.dart';
+import 'package:Eliverd/ui/pages/home.dart';
+
+class MyPagePage extends StatefulWidget {
+  @override
+  _MyPagePageState createState() => _MyPagePageState();
+}
+
+class _MyPagePageState extends State<MyPagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: Header(
+        onBackButtonPressed: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomePage(),
+            ),
+          );
+        },
+        title: '마이페이지',
+      ),
+      body: ListView(
+      ),
+    );
+  }
+}
