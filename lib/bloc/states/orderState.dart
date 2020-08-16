0cbac5109197c5ec8f161a9be9e692e9b2018595
unperknown <unperknown@icloud.com>
2020-08-16 20:04:43 +0900

    feat: order state def

---
+++ lib/bloc/states/orderState.dart
@@ -0,0 +1,45 @@
+import 'package:Eliverd/models/models.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class OrderState extends Equatable {
+  const OrderState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class OrderInitial extends OrderState {}
+
+class OrderInProgress extends OrderState {}
+
+class OrderApproved extends OrderState {
+  final Order order;
+
+  const OrderApproved(this.order);
+
+  @override
+  List<Object> get props => [order];
+}
+
+class OrderCanceled extends OrderState {
+  final Order order;
+
+  const OrderCanceled(this.order);
+
+  @override
+  List<Object> get props => [order];
+}
+
+class OrderFailed extends OrderState {
+  final Order order;
+
+  const OrderFailed(this.order);
+
+  @override
+  List<Object> get props => [order];
+}
+
+class OrderDone extends OrderState {}
+
+class OrderError extends OrderState {}
+
