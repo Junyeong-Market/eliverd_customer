1afd97df8b63d60fc24090980965a732e575b09d
unperknown <unperknown@icloud.com>
2020-08-18 22:32:33 +0900

    refactor: refactor ui change

--- lib/ui/pages/my_page.dart
+++ lib/ui/pages/my_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:Eliverd/ui/widgets/header.dart';
-
 class MyPagePage extends StatefulWidget {
   @override
   _MyPagePageState createState() => _MyPagePageState();
@@ -13,14 +11,47 @@ class _MyPagePageState extends State<MyPagePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: Header(
-        onBackButtonPressed: () {
-          Navigator.pop(context);
-        },
-        title: '마이페이지',
+      appBar: AppBar(
+        leading: ButtonTheme(
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          minWidth: 0,
+          height: 0,
+          child: FlatButton(
+            padding: EdgeInsets.all(0.0),
+            textColor: Colors.black,
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Text(
+              '􀆉',
+              style: TextStyle(
+                fontWeight: FontWeight.w200,
+                fontSize: 24.0,
+              ),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        brightness: Brightness.light,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text(
+          '마이페이지',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        bottom: PreferredSize(
+          child: Container(
+            color: Colors.black12,
+            height: 1.0,
+          ),
+          preferredSize: Size.fromHeight(0.0),
+        ),
       ),
-      body: ListView(
-      ),
+      body: ListView(),
     );
   }
 }
