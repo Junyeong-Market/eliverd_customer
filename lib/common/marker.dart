749a1bceebf568b58ca9b1891730c26f9e83b945
unperknown <unperknown@icloud.com>
2020-08-21 00:34:09 +0900

    refactor: marker icon change

--- lib/common/marker.dart
+++ lib/common/marker.dart
@@ -1,75 +1,20 @@
 import 'dart:typed_data';
-import 'dart:ui';
+import 'dart:ui' as ui;
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Markers {
-  static final int categoryLength = 150;
+  static Future<BitmapDescriptor> getMarkerIcon(String storeName) async {
+    Uint8List marker = await getBytesFromAsset('assets/images/shop.ico', 50);
 
-  static Future<BitmapDescriptor> getMarkerIconByCategories(String storeName,
-      List categories) async {
-    PictureRecorder recorder = PictureRecorder();
+    return BitmapDescriptor.fromBytes(marker);
+  }
 
-    Canvas canvas = Canvas(recorder);
-
-    final markerWidth = categoryLength * categories.length;
-    final markerHeight = categoryLength;
-
-    for (int i = 0; i < categories.length; i++) {
-      TextPainter textPainter = TextPainter(
-        text: TextSpan(
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'SFProText',
-          ),
-          text: categories[i].icon,
-        ),
-        textAlign: TextAlign.center,
-        textDirection: TextDirection.ltr,
-      );
-
-      textPainter.layout();
-
-      final width = categoryLength + (i * categoryLength / 2);
-      final height = categoryLength;
-      final center = Offset(width / 2, height / 2);
-      final radius = categoryLength / 8;
-
-      Paint paintCircle = Paint()..color = categories[i].color;
-
-      canvas.drawCircle(center, radius, paintCircle);
-      textPainter.paint(canvas, Offset(width / 2.35, height / 2.35));
-    }
-
-    TextPainter textPainter = TextPainter(
-      text: TextSpan(
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 30.0,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'SFProText',
-        ),
-        text: storeName,
-      ),
-      textAlign: TextAlign.center,
-      textDirection: TextDirection.ltr,
-    );
-
-    textPainter.layout();
-    textPainter.paint(canvas, Offset(0, 0));
-
-    Picture picture = recorder.endRecording();
-
-    ByteData pngBytes =
-    await (await picture.toImage(markerWidth, markerHeight))
-        .toByteData(format: ImageByteFormat.png);
-
-    Uint8List data = Uint8List.view(pngBytes.buffer);
-
-    return BitmapDescriptor.fromBytes(data);
+  static Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png)).buffer.asUint8List();
   }
 }
