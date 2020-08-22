83f88b30e681a1c6895ce1e6ea4655afe0cb009b
unperknown <unperknown@icloud.com>
2020-08-22 18:55:31 +0900

    feat: destination selection & order request

--- lib/ui/pages/cart.dart
+++ lib/ui/pages/cart.dart
@@ -4,12 +4,20 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:Eliverd/models/models.dart';
 
+import 'package:Eliverd/bloc/events/orderEvent.dart';
+import 'package:Eliverd/bloc/states/orderState.dart';
+import 'package:Eliverd/bloc/orderBloc.dart';
+
 import 'package:Eliverd/ui/widgets/stock.dart';
+import 'package:Eliverd/ui/widgets/search_location.dart';
 
 import 'package:Eliverd/common/color.dart';
 
@@ -20,9 +28,12 @@ class ShoppingCartPage extends StatefulWidget {
 
 class _ShoppingCartPageState extends State<ShoppingCartPage> {
   Future<List<Stock>> cartItems;
+  Future<String> shippingAddress;
   ValueNotifier<List<int>> amounts;
 
   bool isShoppingCartEmpty = true;
+  bool isDelivery = false;
+  Coordinate shippingDestination;
 
   @override
   void initState() {
@@ -72,6 +83,44 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
             fontSize: 20.0,
           ),
         ),
+        actions: [
+          ButtonTheme(
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            minWidth: 0,
+            height: 0,
+            child: FlatButton(
+              padding: EdgeInsets.only(
+                right: 16.0,
+              ),
+              textColor: Colors.black,
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              child: Text(
+                '􀋑',
+                style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  fontSize: 24.0,
+                ),
+                textAlign: TextAlign.right,
+              ),
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) => SearchLocationDialog(
+                    onLocationSelected: _onShippingDestinationSelected,
+                  ),
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       body: FutureBuilder<List<Stock>>(
         future: cartItems,
@@ -147,65 +196,104 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
       bottomNavigationBar: Padding(
         padding: EdgeInsets.all(8.0),
         child: BottomAppBar(
-            color: Colors.transparent,
-            elevation: 0.0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                FutureBuilder<List<Stock>>(
-                  future: cartItems,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-                      return ValueListenableBuilder(
-                        valueListenable: amounts,
-                        builder: (BuildContext context, List<int> amounts,
-                            Widget child) {
-                          int total = 0;
+          color: Colors.transparent,
+          elevation: 0.0,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              FutureBuilder<List<Stock>>(
+                future: cartItems,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.hasData) {
+                    return ValueListenableBuilder(
+                      valueListenable: amounts,
+                      builder: (BuildContext context, List<int> amounts,
+                          Widget child) {
+                        int total = 0;
 
-                          for (int i = 0; i < amounts.length; i++) {
-                            total += snapshot.data[i].price * amounts[i];
-                          }
+                        for (int i = 0; i < amounts.length; i++) {
+                          total += snapshot.data[i].price * amounts[i];
+                        }
 
-                          return Text(
-                            '총합: ${formattedPrice(total)}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 20.0,
-                            ),
-                            textAlign: TextAlign.right,
-                          );
-                        },
-                      );
-                    }
+                        return Text(
+                          '총합: ${formattedPrice(total)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20.0,
+                          ),
+                          textAlign: TextAlign.right,
+                        );
+                      },
+                    );
+                  }
 
+                  return Text(
+                    '총합: ${formattedPrice(0)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20.0,
+                    ),
+                    textAlign: TextAlign.right,
+                  );
+                },
+              ),
+              FutureBuilder<String>(
+                future: shippingAddress,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.hasData) {
                     return Text(
-                      '총합: ${formattedPrice(0)}',
+                      '${snapshot.data}\n로 배송 예정',
                       style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 20.0,
+                        color: Colors.black45,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0,
                       ),
                       textAlign: TextAlign.right,
                     );
-                  },
-                ),
-                SizedBox(height: 4.0),
-                CupertinoButton(
-                  child: Text(
-                    '주문하기',
+                  }
+
+                  return Text(
+                    '배송 없음',
                     style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16.0,
                     ),
-                  ),
-                  color: eliverdColor,
-                  borderRadius: BorderRadius.circular(10.0),
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  onPressed: isShoppingCartEmpty ? null : () {},
-                ),
-              ],
-            )),
+                    textAlign: TextAlign.right,
+                  );
+                },
+              ),
+              SizedBox(height: 4.0),
+              BlocBuilder<OrderBloc, OrderState>(
+                builder: (context, state) {
+                  return CupertinoButton(
+                    child: Text(
+                      '주문하기',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    color: eliverdColor,
+                    borderRadius: BorderRadius.circular(10.0),
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    onPressed: isShoppingCartEmpty
+                        ? null
+                        : () {
+                            cartItems.then((items) => context
+                                .bloc<OrderBloc>()
+                                .add(ProceedOrder(items, amounts.value, isDelivery)));
+                          },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -251,6 +339,29 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
     return carts
         .map((rawProduct) => Stock.fromJson(json.decode(rawProduct)))
         .toList();
+  }
+
+  void _onShippingDestinationSelected(Coordinate coordinate) {
+    final position = LatLng(coordinate.lat, coordinate.lng);
+
+    setState(() {
+      shippingDestination = coordinate;
+      shippingAddress = _getAddressFromPosition(position);
+      isDelivery = true;
+    });
+  }
+
+  Future<String> _getAddressFromPosition(LatLng position) async {
+    List<Placemark> placemarks = await Geolocator().placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+      localeIdentifier: 'ko_KR',
+    );
+
+    return placemarks
+        .map((placemark) =>
+            '${placemark.country} ${placemark.administrativeArea} ${placemark.locality} ${placemark.name} ${placemark.postalCode}')
+        .join(',');
   }
 
   String formattedPrice(int price) {
