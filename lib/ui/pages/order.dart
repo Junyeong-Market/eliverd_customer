bfb88b9a4ab204745df98ef5cffd9ad9e0a63a0d
unperknown <unperknown@icloud.com>
2020-08-22 18:54:52 +0900

    feat: order page

--- lib/ui/pages/order.dart
+++ lib/ui/pages/order.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:Eliverd/ui/widgets/header.dart';
-
 class OrderPage extends StatefulWidget {
+  final String redirectURL;
+
+  const OrderPage({Key key, this.redirectURL}) : super(key: key);
+
   @override
   _OrderPageState createState() => _OrderPageState();
 }
@@ -12,13 +14,45 @@ class _OrderPageState extends State<OrderPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: Header(
-        onBackButtonPressed: () {
-        },
-        title: '결제',
+      appBar: AppBar(
+        leading: ButtonTheme(
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          minWidth: 0,
+          height: 0,
+          child: FlatButton(
+            padding: EdgeInsets.all(0.0),
+            textColor: Colors.black,
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Text(
+              '􀆉',
+              style: TextStyle(
+                fontWeight: FontWeight.w200,
+                fontSize: 24.0,
+              ),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        brightness: Brightness.light,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text(
+          '주문',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
       ),
       body: ListView(
+        children: [
+          Text(
+            widget.redirectURL,
+          ),
+        ],
       ),
     );
   }
