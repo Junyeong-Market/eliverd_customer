944ddac9372973188a195b7db049edba5fa56626
unperknown <unperknown@icloud.com>
2020-08-23 14:21:24 +0900

    feat: navigation to kakaopay & order status

--- lib/ui/pages/order.dart
+++ lib/ui/pages/order.dart
@@ -2,7 +2,15 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import 'package:Eliverd/bloc/events/orderEvent.dart';
+import 'package:Eliverd/bloc/orderBloc.dart';
+import 'package:Eliverd/bloc/states/orderState.dart';
+
+import 'package:Eliverd/ui/pages/order_display.dart';
 
 class OrderPage extends StatefulWidget {
   final String redirectURL;
@@ -19,47 +27,88 @@ class _OrderPageState extends State<OrderPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: ButtonTheme(
-          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          minWidth: 0,
-          height: 0,
-          child: FlatButton(
-            padding: EdgeInsets.all(0.0),
-            textColor: Colors.black,
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-            child: Text(
-              '􀆉',
-              style: TextStyle(
-                fontWeight: FontWeight.w200,
-                fontSize: 24.0,
+    return BlocConsumer<OrderBloc, OrderState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            leading: ButtonTheme(
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              minWidth: 0,
+              height: 0,
+              child: FlatButton(
+                padding: EdgeInsets.all(0.0),
+                textColor: Colors.black,
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                child: Text(
+                  '􀆉',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w200,
+                    fontSize: 24.0,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
             ),
-            onPressed: () {
-              Navigator.pop(context);
+            brightness: Brightness.light,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            title: Text(
+              '주문',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
+          body: WebView(
+            initialUrl: widget.redirectURL,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+            navigationDelegate: (NavigationRequest request) {
+              print(request.url);
+
+              if (request.url.startsWith('kakaotalk')) {
+                _launchURL(request.url);
+
+                return NavigationDecision.prevent;
+              } else if (request.url.contains('SECRET:8000/purchase')) {
+                if (request.url.contains('approve')) {
+                  context.bloc<OrderBloc>().add(ApproveOrder(request.url));
+                } else if (request.url.contains('cancel')) {
+                  context.bloc<OrderBloc>().add(CancelOrder(request.url));
+                } else if (request.url.contains('fail')) {
+                  context.bloc<OrderBloc>().add(FailOrder(request.url));
+                }
+              }
+
+              return NavigationDecision.navigate;
             },
           ),
-        ),
-        brightness: Brightness.light,
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        title: Text(
-          '주문',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w800,
-          ),
-        ),
-      ),
-      body: WebView(
-        initialUrl: widget.redirectURL,
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller.complete(webViewController);
-        },
-      ),
+        );
+      },
+      listener: (context, state) {
+        if (state is! OrderInProgress) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => OrderDisplayPage(),
+            ),
+          );
+        }
+      },
     );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
