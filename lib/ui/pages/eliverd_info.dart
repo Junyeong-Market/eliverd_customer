0c15d2f813f96c4edadd50022c7e85a1b8e50833
unperknown <unperknown@icloud.com>
2020-08-24 15:17:57 +0900

    feat: eliverd info template

--- lib/ui/pages/eliverd_info.dart
+++ lib/ui/pages/eliverd_info.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:Eliverd/common/color.dart';
+
 class EliverdInfoPage extends StatefulWidget {
   @override
   _EliverdInfoPageState createState() => _EliverdInfoPageState();
@@ -9,6 +11,9 @@ class EliverdInfoPage extends StatefulWidget {
 class _EliverdInfoPageState extends State<EliverdInfoPage> {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         leading: ButtonTheme(
@@ -43,7 +48,138 @@ class _EliverdInfoPageState extends State<EliverdInfoPage> {
           ),
         ),
       ),
-      body: ListView(),
+      body: ListView(
+        padding: EdgeInsets.all(24.0),
+        children: [
+          Container(
+            width: width,
+            height: height * 0.25,
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [eliverdLightColor, eliverdDarkColor],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight),
+              borderRadius: BorderRadius.all(Radius.circular(25.0)),
+            ),
+            child: Text(
+              '누가 봐도 오늘의 대표 상품 소개하는 배너',
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+                fontSize: 28.0,
+              ),
+            ),
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            '카테고리 살펴보기',
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+              fontSize: 28.0,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            width: width,
+            height: 120.0,
+            child: GridView(
+              scrollDirection: Axis.horizontal,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                mainAxisSpacing: 8.0,
+              ),
+              children: <Widget>[
+                Container(
+                  width: width,
+                  height: 120.0,
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  ),
+                  child: Text(
+                    '음식',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: width,
+                  height: 120.0,
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  ),
+                  child: Text(
+                    '생활용품',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: width,
+                  height: 120.0,
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange,
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  ),
+                  child: Text(
+                    '음반',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: width,
+                  height: 120.0,
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  ),
+                  child: Text(
+                    '의류',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: width,
+                  height: 120.0,
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.pinkAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  ),
+                  child: Text(
+                    '식물',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
