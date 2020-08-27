d2e49169248e3037860dd878aecfc5681f419188
unperknown <unperknown@icloud.com>
2020-08-27 21:19:03 +0900

    fix: remove cart items as navigate to

--- lib/ui/pages/order_display.dart
+++ lib/ui/pages/order_display.dart
@@ -1,26 +1,27 @@
+
+import 'package:Eliverd/common/color.dart';
+import 'package:Eliverd/ui/pages/order.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:Eliverd/bloc/events/orderEvent.dart';
 import 'package:Eliverd/bloc/orderBloc.dart';
 import 'package:Eliverd/bloc/states/orderState.dart';
+import 'package:Eliverd/models/models.dart';
 
 import 'package:Eliverd/ui/widgets/order.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OrderDisplayPage extends StatefulWidget {
+  final Order order;
+
+  const OrderDisplayPage({Key key, this.order}) : super(key: key);
+
   @override
   _OrderDisplayPageState createState() => _OrderDisplayPageState();
 }
 
 class _OrderDisplayPageState extends State<OrderDisplayPage> {
-  @override
-  void initState() {
-    super.initState();
-
-    _removeShoppingCart();
-  }
-
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<OrderBloc, OrderState>(
@@ -52,54 +53,63 @@ class _OrderDisplayPageState extends State<OrderDisplayPage> {
             backgroundColor: Colors.transparent,
             elevation: 0.0,
             title: Text(
-              '주문 ${state is OrderApproved ? '완료' : (state is OrderCanceled ? '취소' : (state is OrderFailed ? '실패' : '중 오류 발생'))}',
+              '주문 상세 조회',
               style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w800,
               ),
             ),
           ),
-          body: state is OrderApproved
-              ? ListView(
-                  children: [
-                    OrderWidget(order: state.order),
-                  ],
-                )
-              : (state is OrderCanceled
-                  ? ListView(
-                      children: [
-                        OrderWidget(order: state.order),
-                      ],
-                    )
-                  : (state is OrderFailed
-                      ? ListView(
-                          children: [
-                            OrderWidget(order: state.order),
-                          ],
-                        )
-                      : Center(
-                          child: Text(
-                            '주문 중 예기치 않은 오류가 발생했습니다.\n나중에 다시 시도해주세요.',
-                            style: TextStyle(
-                              color: Colors.black26,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ))),
+          body: ListView(
+            children: [
+              OrderWidget(
+                order: widget.order,
+              ),
+            ],
+          ),
+          bottomNavigationBar: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: BottomAppBar(
+              color: Colors.transparent,
+              elevation: 0.0,
+              child: CupertinoButton(
+                child: Text(
+                  '재주문',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+                color: eliverdColor,
+                borderRadius: BorderRadius.circular(10.0),
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                onPressed: widget.order.status == 'processed'
+                    ? null
+                    : () {
+                        context.bloc<OrderBloc>().add(
+                              ContinueOrder(
+                                widget.order.id,
+                              ),
+                            );
+                      },
+              ),
+            ),
+          ),
         );
       },
       listener: (context, state) {
-        if (state is OrderInProgress) {
-          Navigator.pop(context);
+        if (state is OrderInResume) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => OrderPage(
+                redirectURL: state.redirectURL,
+              ),
+            ),
+          );
         }
       },
     );
-  }
-
-  Future<void> _removeShoppingCart() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    prefs.remove('carts');
   }
 }
