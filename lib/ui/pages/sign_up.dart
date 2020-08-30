91f49853fcdce1ba8bf70d33fdb226503f414dc5
unperknown <unperknown@icloud.com>
2020-08-30 15:18:21 +0900

    refactor: updated sign up ui

--- lib/ui/pages/sign_up.dart
+++ lib/ui/pages/sign_up.dart
@@ -54,6 +54,8 @@ class _SignUpPageState extends State<SignUpPage> {
         ),
       ),
     );
+
+    _realnameNavigationFocus.requestFocus();
   }
 
   @override
@@ -70,10 +72,7 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-
-    _realnameNavigationFocus.requestFocus();
 
     return BlocProvider<AccountBloc>.value(
       value: _accountBloc,
@@ -86,14 +85,17 @@ class _SignUpPageState extends State<SignUpPage> {
         builder: (context, state) {
           return Scaffold(
             key: SignUpPageKeys.signUpPage,
-            appBar: _brightAppBar,
+            appBar: _buildAppBar(context),
             body: ListView(
+              physics: NeverScrollableScrollPhysics(),
               padding: EdgeInsets.symmetric(
-                horizontal: width * 0.075,
+                horizontal: 24.0,
               ),
               children: <Widget>[
-                _signUpTitle,
-                SizedBox(height: height / 48.0),
+                _buildTitle(),
+                SizedBox(
+                  height: 16.0,
+                ),
                 _buildRealnameSection(state, height),
                 _buildNicknameSection(state, height),
                 _buildUserIdSection(state, height),
@@ -103,8 +105,7 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
             bottomNavigationBar: Padding(
               padding: EdgeInsets.symmetric(
-                horizontal: width * 0.075,
-                vertical: 15.0,
+                horizontal: 8.0,
               ),
               child: _buildSignUpBtn(),
             ),
@@ -114,8 +115,29 @@ class _SignUpPageState extends State<SignUpPage> {
     );
   }
 
-  final _brightAppBar = AppBar(
+  Widget _buildAppBar(BuildContext context) => AppBar(
     backgroundColor: Colors.transparent,
+    leading: ButtonTheme(
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      minWidth: 0,
+      height: 0,
+      child: FlatButton(
+        padding: EdgeInsets.all(0.0),
+        textColor: Colors.black,
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        child: Text(
+          '􀆉',
+          style: TextStyle(
+            fontWeight: FontWeight.w200,
+            fontSize: 24.0,
+          ),
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    ),
     brightness: Brightness.light,
     elevation: 0.0,
     iconTheme: IconThemeData(color: Colors.black),
@@ -138,7 +160,7 @@ class _SignUpPageState extends State<SignUpPage> {
     allow: true,
   );
 
-  Widget _signUpTitle = Text(
+  Widget _buildTitle() => Text(
     TitleStrings.signUpTitle,
     style: const TextStyle(
       color: Colors.black,
@@ -148,232 +170,256 @@ class _SignUpPageState extends State<SignUpPage> {
   );
 
   Widget _buildRealnameSection(AccountState state, double height) => Visibility(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            FormText(
-              controller: _nameController,
-              textWhenCompleted: SignUpStrings.realnameDesc,
-              textWhenNotCompleted: SignUpStrings.realnameDescWhenImcompleted,
-            ),
-            SizedBox(height: height / 120.0),
-            FormTextField(
-              key: SignUpPageKeys.realnameTextField,
-              regex: _nameRegex,
-              maxLength: 128,
-              isObscured: false,
-              controller: _nameController,
-              isEnabled: !_isRealnameSubmitted,
-              helperText: SignUpStrings.realnameHelperText,
-              errorMessage: getErrorMessage(
-                  state, 'realname', ErrorMessages.realnameInvalidMessage),
-              focusNode: _realnameNavigationFocus,
-              onSubmitted: (_) {
-                setState(() {
-                  _isRealnameSubmitted = true;
-                });
-
-                _nicknameNavigationFocus.requestFocus();
-              },
-            ),
-            SizedBox(height: height / 120.0),
-          ],
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        FormText(
+          controller: _nameController,
+          textWhenCompleted: SignUpStrings.realnameDesc,
+          textWhenNotCompleted: SignUpStrings.realnameDescWhenImcompleted,
         ),
-        maintainSize: true,
-        maintainAnimation: true,
-        maintainState: true,
-        visible: true,
-      );
+        SizedBox(
+          height: 8.0,
+        ),
+        FormTextField(
+          key: SignUpPageKeys.realnameTextField,
+          regex: _nameRegex,
+          maxLength: 128,
+          isObscured: false,
+          controller: _nameController,
+          isEnabled: !_isRealnameSubmitted,
+          helperText: SignUpStrings.realnameHelperText,
+          errorMessage: getErrorMessage(
+              state, 'realname', ErrorMessages.realnameInvalidMessage),
+          focusNode: _realnameNavigationFocus,
+          onSubmitted: (_) {
+            if (_nameController.text.length != 0) {
+              setState(() {
+                _isRealnameSubmitted = true;
+              });
+
+              _nicknameNavigationFocus.requestFocus();
+            }
+
+            // TO-DO: 이름 미입력 시 Snackbar 띄우기
+          },
+        ),
+      ],
+    ),
+    maintainSize: true,
+    maintainAnimation: true,
+    maintainState: true,
+    visible: true,
+  );
 
   Widget _buildNicknameSection(AccountState state, double height) => Visibility(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            FormText(
-              controller: _nicknameController,
-              textWhenCompleted: SignUpStrings.nicknameDesc,
-              textWhenNotCompleted: SignUpStrings.nicknameDescWhenImcompleted,
-            ),
-            SizedBox(height: height / 120.0),
-            FormTextField(
-              key: SignUpPageKeys.nicknameTextField,
-              regex: _nicknameRegex,
-              maxLength: 50,
-              isObscured: false,
-              controller: _nicknameController,
-              helperText: SignUpStrings.nicknameHelperText,
-              isEnabled: !_isNicknameSubmitted,
-              errorMessage: getErrorMessage(
-                  state,
-                  'nickname',
-                  ErrorMessages.nicknameInvalidMessage,
-                  ErrorMessages.nicknameDuplicatedMessage),
-              focusNode: _nicknameNavigationFocus,
-              onSubmitted: (_) {
-                setState(() {
-                  _isNicknameSubmitted = true;
-                });
-
-                _userIdNavigationFocus.requestFocus();
-              },
-            ),
-            SizedBox(height: height / 120.0),
-          ],
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        FormText(
+          controller: _nicknameController,
+          textWhenCompleted: SignUpStrings.nicknameDesc,
+          textWhenNotCompleted: SignUpStrings.nicknameDescWhenImcompleted,
         ),
-        maintainSize: true,
-        maintainAnimation: true,
-        maintainState: true,
-        visible: _isRealnameSubmitted,
-      );
+        SizedBox(
+          height: 8.0,
+        ),
+        FormTextField(
+          key: SignUpPageKeys.nicknameTextField,
+          regex: _nicknameRegex,
+          maxLength: 50,
+          isObscured: false,
+          controller: _nicknameController,
+          helperText: SignUpStrings.nicknameHelperText,
+          isEnabled: !_isNicknameSubmitted,
+          errorMessage: getErrorMessage(
+              state,
+              'nickname',
+              ErrorMessages.nicknameInvalidMessage,
+              ErrorMessages.nicknameDuplicatedMessage),
+          focusNode: _nicknameNavigationFocus,
+          onSubmitted: (_) {
+            if (_nicknameController.text.length != 0) {
+              setState(() {
+                _isNicknameSubmitted = true;
+              });
+
+              _userIdNavigationFocus.requestFocus();
+            } else {
+              _nicknameNavigationFocus.requestFocus();
+            }
+
+            // TO-DO: 닉네임 미입력 시 Snackbar 띄우기
+          },
+        ),
+      ],
+    ),
+    maintainSize: true,
+    maintainAnimation: true,
+    maintainState: true,
+    visible: _isRealnameSubmitted,
+  );
 
   Widget _buildUserIdSection(AccountState state, double height) => Visibility(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            FormText(
-              controller: _userIdController,
-              textWhenCompleted: SignUpStrings.idDesc,
-              textWhenNotCompleted: SignUpStrings.idDescWhenImcompleted,
-            ),
-            SizedBox(height: height / 120.0),
-            FormTextField(
-              key: SignUpPageKeys.idTextField,
-              regex: _idRegex,
-              maxLength: 50,
-              isObscured: false,
-              controller: _userIdController,
-              helperText: SignUpStrings.userIdHelperText,
-              isEnabled: !_isUserIdSubmitted,
-              errorMessage: getErrorMessage(
-                  state,
-                  'userId',
-                  ErrorMessages.userIdInvalidMessage,
-                  ErrorMessages.userIdDuplicatedMessage),
-              focusNode: _userIdNavigationFocus,
-              onSubmitted: (_) {
-                setState(() {
-                  _isUserIdSubmitted = true;
-                });
-
-                _passwordNavigationFocus.requestFocus();
-              },
-            ),
-            SizedBox(height: height / 120.0),
-          ],
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        FormText(
+          controller: _userIdController,
+          textWhenCompleted: SignUpStrings.idDesc,
+          textWhenNotCompleted: SignUpStrings.idDescWhenImcompleted,
         ),
-        maintainSize: true,
-        maintainAnimation: true,
-        maintainState: true,
-        visible: _isNicknameSubmitted,
-      );
+        SizedBox(
+          height: 8.0,
+        ),
+        FormTextField(
+          key: SignUpPageKeys.idTextField,
+          regex: _idRegex,
+          maxLength: 50,
+          isObscured: false,
+          controller: _userIdController,
+          helperText: SignUpStrings.userIdHelperText,
+          isEnabled: !_isUserIdSubmitted,
+          errorMessage: getErrorMessage(
+              state,
+              'userId',
+              ErrorMessages.userIdInvalidMessage,
+              ErrorMessages.userIdDuplicatedMessage),
+          focusNode: _userIdNavigationFocus,
+          onSubmitted: (_) {
+            if (_userIdController.text.length != 0) {
+              setState(() {
+                _isUserIdSubmitted = true;
+              });
+
+              _passwordNavigationFocus.requestFocus();
+            } else {
+              _userIdNavigationFocus.requestFocus();
+            }
+          },
+        ),
+      ],
+    ),
+    maintainSize: true,
+    maintainAnimation: true,
+    maintainState: true,
+    visible: _isNicknameSubmitted,
+  );
 
   Widget _buildPasswordSection(AccountState state, double height) => Visibility(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            FormText(
-              controller: _passwordController,
-              textWhenCompleted: SignUpStrings.passwordDesc,
-              textWhenNotCompleted: SignUpStrings.passwordDescWhenImcompleted,
-            ),
-            SizedBox(height: height / 120.0),
-            FormTextField(
-              key: SignUpPageKeys.passwordTextField,
-              maxLength: 256,
-              isObscured: true,
-              helperText: SignUpStrings.passwordHelperText,
-              controller: _passwordController,
-              isEnabled: !_isPasswordSubmitted,
-              regex: _passwordRegex,
-              errorMessage: getErrorMessage(
-                  state, 'password', ErrorMessages.passwordInvalidMessage),
-              focusNode: _passwordNavigationFocus,
-              onSubmitted: (value) {
-                setState(() {
-                  _isPasswordSubmitted = true;
-                });
-
-                _passwordNavigationFocus.unfocus();
-              },
-            ),
-            SizedBox(height: height / 120.0),
-          ],
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        FormText(
+          controller: _passwordController,
+          textWhenCompleted: SignUpStrings.passwordDesc,
+          textWhenNotCompleted: SignUpStrings.passwordDescWhenImcompleted,
         ),
-        maintainSize: true,
-        maintainAnimation: true,
-        maintainState: true,
-        visible: _isUserIdSubmitted,
-      );
+        SizedBox(
+          height: 8.0,
+        ),
+        FormTextField(
+          key: SignUpPageKeys.passwordTextField,
+          maxLength: 256,
+          isObscured: true,
+          helperText: SignUpStrings.passwordHelperText,
+          controller: _passwordController,
+          isEnabled: !_isPasswordSubmitted,
+          regex: _passwordRegex,
+          errorMessage: getErrorMessage(
+              state, 'password', ErrorMessages.passwordInvalidMessage),
+          focusNode: _passwordNavigationFocus,
+          onSubmitted: (value) {
+            if (_passwordController.text.length != 0) {
+              setState(() {
+                _isPasswordSubmitted = true;
+              });
+
+              _passwordNavigationFocus.unfocus();
+            } else {
+              _passwordNavigationFocus.requestFocus();
+            }
+          },
+        ),
+      ],
+    ),
+    maintainSize: true,
+    maintainAnimation: true,
+    maintainState: true,
+    visible: _isUserIdSubmitted,
+  );
 
   Widget _buildIsSellerSection(AccountState state, double height) => Visibility(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  SignUpStrings.isSellerDesc,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 26.0,
-                  ),
-                ),
-                CupertinoSwitch(
-                  key: SignUpPageKeys.isSellerSwitch,
-                  value: _isSeller,
-                  onChanged: (value) {
-                    setState(() {
-                      _isSeller = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            SizedBox(height: height / 120.0),
-            Visibility(
-              key: SignUpPageKeys.signUpErrorMsg,
-              child: Text(
-                ErrorMessages.signUpErrorMessage,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold,
-                ),
+            Text(
+              SignUpStrings.isSellerDesc,
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 26.0,
               ),
-              maintainSize: false,
-              maintainAnimation: true,
-              maintainState: true,
-              visible: state is AccountError,
+            ),
+            CupertinoSwitch(
+              key: SignUpPageKeys.isSellerSwitch,
+              value: _isSeller,
+              onChanged: (value) {
+                setState(() {
+                  _isSeller = value;
+                });
+              },
             ),
           ],
         ),
-        maintainSize: true,
-        maintainAnimation: true,
-        maintainState: true,
-        visible: _isPasswordSubmitted,
-      );
-
-  Widget _buildSignUpBtn() => BottomAppBar(
-        color: Colors.transparent,
-        elevation: 0.0,
-        child: CupertinoButton(
-          key: SignUpPageKeys.signUpBtn,
+        SizedBox(
+          height: 8.0,
+        ),
+        Visibility(
+          key: SignUpPageKeys.signUpErrorMsg,
           child: Text(
-            SignUpStrings.signUpButtonDesc,
+            ErrorMessages.signUpErrorMessage,
             style: TextStyle(
-              color: Colors.white,
+              color: Colors.red,
               fontWeight: FontWeight.bold,
-              fontSize: 16.0,
             ),
           ),
-          color: eliverdColor,
-          borderRadius: BorderRadius.circular(25.0),
-          padding: EdgeInsets.symmetric(vertical: 15.0),
-          onPressed: _passwordController.text.length != 0
-              ? validateAndSignUpUser
-              : null,
+          maintainSize: false,
+          maintainAnimation: true,
+          maintainState: true,
+          visible: state is AccountError,
         ),
-      );
+      ],
+    ),
+    maintainSize: true,
+    maintainAnimation: true,
+    maintainState: true,
+    visible: _isPasswordSubmitted,
+  );
+
+  Widget _buildSignUpBtn() => BottomAppBar(
+    color: Colors.transparent,
+    elevation: 0.0,
+    child: CupertinoButton(
+      key: SignUpPageKeys.signUpBtn,
+      child: Text(
+        SignUpStrings.signUpButtonDesc,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 19.0,
+        ),
+      ),
+      color: eliverdColor,
+      borderRadius: BorderRadius.circular(10.0),
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      onPressed: _passwordController.text.length != 0
+          ? validateAndSignUpUser
+          : null,
+    ),
+  );
 
   String getErrorMessage(
       AccountState state, String fieldName, String invalidMsg,
